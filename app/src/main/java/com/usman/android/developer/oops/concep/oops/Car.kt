91e2f4b49582e033fa4b1997f947d67a0477bf30
muhammaduthman688 <muhammadusman688@gmail.com>
2025-04-26 19:51:54 +0500

    first Commit

---
+++ app/src/main/java/com/usman/android/developer/oops/concep/oops/Car.kt
@@ -0,0 +1,23 @@
+package com.usman.android.developer.oops.concep.oops
+
+class Car(val model:String,val color:String) {
+    fun start(){
+        println("$model is Starting")
+
+    }
+    fun stop(){
+        println("$model is Stopping")
+    }
+    fun color(){
+        println("$model is $color")
+    }
+}
+
+fun main(){
+    val myCar=Car("Toyota","Red")
+
+    myCar.start()
+    myCar.stop()
+    myCar.color()
+
+}
